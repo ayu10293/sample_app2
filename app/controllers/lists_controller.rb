3ae41c2a9d29c0c7b47c8f490786d3100e590719
ayu <133560587+ayu10293@users.noreply.github.com@users.noreply.github.com>
2023-07-08 01:54:18 +0000

    [finish]3-9]

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,9 +4,13 @@ class ListsController < ApplicationController
   end
 
   def create
-    list = List.new(list_params)
-    list.save
+    @list = List.new(list_params)
+    if @list.save
     redirect_to list_path(list.id)
+    else
+      #redirect_to new_list_path
+    render :new
+    end
   end
 
   def index
@@ -18,10 +22,24 @@ class ListsController < ApplicationController
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
   end
 
   private
+
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
